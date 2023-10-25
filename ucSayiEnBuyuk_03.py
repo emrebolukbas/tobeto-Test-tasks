ef7b53cf2660beb72b00ae8eed316808c9d9035f
emrebolukbas <yunusemre.bolukbas@hotmail.com>
2023-10-25 18:38:35 +0300

    esitlikDurumuGuncelleme

--- ucSayiEnBuyuk_03.py
+++ ucSayiEnBuyuk_03.py
@@ -3,9 +3,12 @@ sayi1 = int(input("Lutfen ilk sayıyı giriniz: "))
 sayi2 = int(input("Lutfen ikinci sayıyı giriniz: "))
 sayi3 = int(input("Lutfen üçüncü sayıyı giriniz: "))
 
-if sayi1>sayi2 and sayi1>sayi3:
-    print("En büyük sayı :", sayi1)
-elif sayi2>sayi3:
-    print("En büyük sayı :",sayi2)
+if sayi1==sayi2==sayi3:
+    print("Tekrar eden sayilar var")
 else:
-    print("En büyük sayı :",sayi3)
+    if sayi1>sayi2 and sayi1>sayi3:
+        print("En büyük sayı :", sayi1)
+    elif sayi2>sayi3:
+        print("En büyük sayı :",sayi2)
+    else:
+      print("En büyük sayı :",sayi3)
