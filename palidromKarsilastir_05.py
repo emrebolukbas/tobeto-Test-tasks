98ee2dc94a1c7bff4d32e95652dd28bb18fd4d7a
emrebolukbas <yunusemre.bolukbas@hotmail.com>
2023-10-26 19:09:51 +0300

    isim değiştirildiği için silindi

--- palidromKarsilastir_05.py
+++
@@ -1,8 +0,0 @@
-#5-Kullanıcıdan alınan bir sayının palindrom olup olmadığını bulan bir program yazınız.
-isim = str(input("Palidrom Bir Kelime Giriniz :"))
-tersi=isim[::-1]
-
-if isim==tersi:
-    print("Girdiğiniz İsim :",isim,"\nTersten hali :",tersi,"\n Bu isim bir palidromdur...")
-else:
-    print("Girdiğiniz İsim :",isim,"\nTersten hali :",tersi,"\n Bu isim bir palidrom değildir...")
--- vucutKitleIndeksi_01.py
+++
@@ -1,5 +0,0 @@
-#1-Kullanıcının girdiği boy ve ağırlık değerlerine göre vücut kitle indeksini (VKİ = ağırlık/(boy*boy)) hesaplayınız.
-boy = float(input("Lütfen boyunuzu metre cinsinden giriniz: "))
-kilo = float(input("Lütfen kilonuzu giriniz: "))
-vki = kilo/(boy*boy)
-print("Vücut kitle indeksiniz :", vki)
